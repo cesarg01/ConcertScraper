08323d40d5c741d633341f59f20ca8ab53244a57
Cesar Gonzalez <gbrothers93@gmail.com>
2018-11-01 01:05:47 -0700

    Created methods for location temperature. Fixed a bug where the program will throw an error when it did not find the artist on the website. Program will now ask the user for suggestion when it can't find the artist.

--- concert_scraper.py
+++ concert_scraper.py
@@ -45,8 +45,31 @@ def get_tour_dates(artist_site_soup, new_dates):
         #print(head + '\n')
     return new_dates
 
+def get_loc_temp(place):
+    gmaps = googlemaps.Client(key='AIzaSyBeDHc2IX4OFFAUnwMuNG93fJbd52_K274')
+    geocode_result = gmaps.geocode(place)
+    lat = geocode_result[0]["geometry"]["location"]["lat"]
+    lon = geocode_result[0]["geometry"]["location"]["lng"]
+    #test - print results
+    #print (lat,lon)
+    # API key for OWM
+    owm = pyowm.OWM('21b71550850fc524c2e46097ba41d774')
+    observation = owm.weather_at_coords(lat,lon)
+    w = observation.get_weather()
+    #print(w)
+    
+    temperature = w.get_temperature('fahrenheit')['temp']
+    print(temperature)
+    print(w.get_detailed_status())
+    fc = owm.daily_forecast(place, limit = 1)
+    f = fc.get_forecast()
+    print(fc.will_have_rain())
+    print(fc.will_have_snow())
+    print(fc.will_have_fog())
+    
+    return temperature
 
-
+        
 ################################################## MAIN PROGRAM #########################################################
 
 # URL used to find where the artist or band will be playing.
@@ -57,6 +80,7 @@ artist_name = input('Please enter the name of the artist. \n')
 artist = checkSpace(artist_name)
 
 songkick_site = songkick_site + artist
+#print(songkick_site)
 songkick_site_object = requests.get(songkick_site)
 songkick_site_text = songkick_site_object.text
 
@@ -64,8 +88,14 @@ songkick_site_soup = BeautifulSoup(songkick_site_text, 'html.parser')
 #print(songkick_site_soup)
 
 # Find the artist id from the HTML tag: <input type="hidden" name="subject_id" value="some number">
-artist_id = songkick_site_soup.find('input', {'name': 'subject_id'})['value']
-print("Artist ID: " , artist_id)
+while True:
+    try:
+        artist_id = songkick_site_soup.find('input', {'name': 'subject_id'})['value']
+        print("Artist ID: " , artist_id)
+        break
+    except AttributeError:
+        print('Site has no artist ID for {}'.format(artist_name))
+        exit()
 
 # Find the link to the artist's site on songkick.
 link_found = []
@@ -88,7 +118,19 @@ print(new_link)
 #print(new_link)
 
 artist_site = songkick_site[:24] + new_link
-print(artist_site)
+if(artist_site == songkick_site[:24]):
+    alternative_link = '/artists/' + artist_id + '-' + artist
+    print('Artist your looking for not on website. Please try again by removing words like "the".')
+    answer = input('Do you want to check if {} is the correct artist link you are looling for? If so please type yes, y, no, or n. \n'.format(alternative_link))
+    answer = answer.lower()
+    if(answer == 'yes' or answer == 'y'):
+        artist_site = songkick_site[:24] + alternative_link
+    else:
+        print('Artist your looking for not on website. Please try again by removing words like "the".')
+        exit()
+else:
+    print(artist_site)
+
 
 # Get request from the artist site and parse it
 artist_site_object = requests.get(artist_site)
@@ -96,42 +138,38 @@ artist_site_text = artist_site_object.text
 artist_site_soup = BeautifulSoup(artist_site_text, 'html.parser')
 
 # Check if the artist is on tour so we can get more info
-on_tour = artist_site_soup.find('li', class_ = 'ontour').get_text()
-print(on_tour)
+while True:
+    try:
+        on_tour = artist_site_soup.find('li', class_ = 'ontour').get_text()
+        print(on_tour)
+        break
+    except AttributeError:
+        print('Site has no information for {}'.format(artist_name))
+        exit()
 
+
+# Check if the artist is on tour. If so get the tour dates.
 if('yes' in on_tour):
     dates = []
-    get_tour_dates(artist_site_soup, dates)
-    for date in dates:
-        print(date)
-        if(len(dates) == 0):
-            exit
-        else:
-            # Get the city and state and check the tempature and the current condition
-            location = dates[0].split(', ')
-            print(location)
-        
-            city = location[len(location)-2]
-            state = location[len(location)-1]
-            place = city + ', ' + state + ', USA'
-            print(place)
-
-            gmaps = googlemaps.Client(key='AIzaSyBeDHc2IX4OFFAUnwMuNG93fJbd52_K274')
-            geocode_result = gmaps.geocode(place)
-            lat = geocode_result[0]["geometry"]["location"]["lat"]
-            lon = geocode_result[0]["geometry"]["location"]["lng"]
-            #test - print results
-            #print (lat,lon)
-            # API key for OWM
-            owm = pyowm.OWM('21b71550850fc524c2e46097ba41d774')
-            observation = owm.weather_at_coords(lat,lon)
-            w = observation.get_weather()
-            #print(w)
-            
-            temperature = w.get_temperature('fahrenheit')['temp']
-            print(temperature)
-            print(w.get_detailed_status())
-        
+    dates = get_tour_dates(artist_site_soup, dates)
+    if(len(dates) == 0):
+            exit()
     else:
-        print('{} is not on tour.'.format(artist_name))
-        exit
+        for date in dates:
+            print(date)
+
+        # Get the city and state and check the tempature and the current condition
+        location = dates[0].split(', ')
+        #print(location)
+        
+        city = location[len(location)-2]
+        state = location[len(location)-1]
+        place = city + ', ' + state + ', USA'
+        print(place)
+
+        temp = get_loc_temp(place)
+        print(temp)
+    
+else:
+    print('{} is not on tour.'.format(artist_name))
+    exit()
